84df95a47b82b1df67c509513b0f3d0274f5d13c
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-04-18 19:51:22 -0300

    adjusted to fetch one word and set a state with all attributes needed

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { useCallback, useEffect, useState } from "react";
 
 //data - Words - use open dictionary api to get
 import { getRandomWord, WordData } from "./api/api";
+import { xml2json } from "xml-js";
 
 //components
 import StarterScreen from "./components/StarterScreen";
@@ -19,11 +20,33 @@ const stages = [
 ];
 
 function App() {
-  const [randomWord, setRandomWord] = useState<WordData>();
+  const [randomWord, setRandomWord] = useState<{word: string; def: string; arrLetter: string[]}>();
   const [gameStage, setGameStage ] = useState(stages[0].name);
+
+  //fetch a random word from the api
+  const fetchRandomWord = async () => {
+    try{
+      const fetchedWordData = await getRandomWord();
+      const wordJson = JSON.parse(xml2json(fetchedWordData.xml, {compact:true, spaces: 2}));
+      const newWord = wordJson.entry._attributes.id as string;
+      const defWord = wordJson.entry.sense.def._text;
+      const arrLetter = newWord.toUpperCase().split("");
+      setRandomWord({word: newWord, def: defWord, arrLetter: arrLetter});
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchRandomWord();
+  }, []);
 
   //starts the secret word game
   const startGame = () => {
+    console.log(randomWord);
+    if(randomWord?.word !== undefined){
+      fetchRandomWord();
+    }
     setGameStage(stages[1].name);
   }
 
@@ -36,20 +59,6 @@ function App() {
   const retry = () => {
     setGameStage(stages[0].name);
   }
-
-  useEffect(() => {
-    const fetchRandomWord = async () => {
-      try{
-        const fetchedWordData: WordData = await getRandomWord();
-        setRandomWord(fetchedWordData)
-      }catch(err){
-        console.log(err);
-      }
-    }
-
-    //on reload it returns as undefined
-    fetchRandomWord(); 
-  }, []);
 
   return (
     <div className="App">
--- src/api/api.ts
+++ src/api/api.ts
@@ -5,18 +5,18 @@
 
 export interface WordData{
     word: string;
-    deletor?: string;
-    revision_id?: number;
-    modetator?: string;
-    xml?:  string;
+    deletor: string;
+    revision_id: number;
+    modetator: string;
+    xml:  string;
     deleted?: number;
-    last_revision?: number;
-    sense?: number;
-    creator?: string;
-    timestamp?: string;
-    normalized?: string;
-    derived_from?: string;
-    word_id?: number;
+    last_revision: number;
+    sense: number;
+    creator: string;
+    timestamp: string;
+    normalized: string;
+    derived_from: string;
+    word_id: number;
 }
 
 export const getRandomWord = async (): Promise<WordData> => {
